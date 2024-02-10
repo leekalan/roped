a8270ee4a402633d78b7aa5baf021f1f9f5be865
Flamearoo <kalanlee7258@gmail.com>
2024-02-10 11:34:16 +1100

    Delete bundle-derive/target directory

--- bundle-derive/target/package/bundle-derive-0.4.0/target/debug/build/proc-macro2-66714acbfb70873f/out/proc_macro2.d
+++
@@ -1,7 +0,0 @@
-C:\Users\kalan\Desktop\Coding\rope\bundle-derive\target\package\bundle-derive-0.4.0\target\debug\build\proc-macro2-66714acbfb70873f\out\libproc_macro2.rmeta: build\probe.rs
-
-C:\Users\kalan\Desktop\Coding\rope\bundle-derive\target\package\bundle-derive-0.4.0\target\debug\build\proc-macro2-66714acbfb70873f\out\proc_macro2.d: build\probe.rs
-
-build\probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
